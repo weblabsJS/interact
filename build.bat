e9a4e4fa7873706c96b9a20d432aa18e42f36eb8
DevMayukh-STRTM <kismotechindia@gmail.com>
2023-08-04 11:56:26 +0530

    updated build.bat to support the script building function

--- build.bat
+++ build.bat
@@ -1,32 +1,22 @@
 @echo off
 
-:: Create dist folder
 mkdir dist
-
-:: Compile TypeScript
-tsc -p tsconfig.json
-
-:: Copy necessary files to dist
-copy src\package.json dist
-copy src\style.css dist
-copy README.MD dist
-
-:: Add files to Git staging
-git add src\package.json
-git add src\style.css
+tsc -p ./tsconfig.json
+copy ./src/package.json ./dist
+python scripts/style.py
+copy README.MD ./dist
+git add ./src/package.json
+git add ./src/style.css
 git add build.bat
 git add README.MD
 git add *
 git add .gitignore
 
-:: Prompt for commit message
-set /p commit_message=Enter the commit message: 
+REM Prompt the user for the commit message
+set /p commit_message="Enter the commit message: "
 
-:: Commit changes
 git commit -m "%commit_message%"
 git push
 
-:: For now, we will not push anything to the npm servers.
-:: Uncomment the next line if you want to run npm publish
-:: cd dist
-:: npm publish --access public
+REM For now, we will not push anything to the npm servers.
+REM cd dist && npm publish --access public
